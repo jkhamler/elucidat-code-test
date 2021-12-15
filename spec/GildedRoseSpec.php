3b301828b7b62130849ec2b8697e6bce58683862
jonathanhamler <jkhamler@gmail.com>
2021-12-15 17:55:44 +0000

    Adding conjured property

--- spec/GildedRoseSpec.php
+++ spec/GildedRoseSpec.php
@@ -1,7 +1,7 @@
 <?php
 
-use App\Item;
 use App\GildedRose;
+use App\Item;
 
 describe('Gilded Rose', function () {
     describe('next day', function () {
@@ -151,45 +151,43 @@ describe('Gilded Rose', function () {
                 expect($gr->getItem(0)->sellIn)->toBe(-2);
             });
         });
-        /*
         context("Conjured Items", function () {
             it('updates Conjured items before the sell date', function () {
                 $gr = new GildedRose([new Item('Conjured Mana Cake', 10, 10)]);
-                $gr->nextDay();
+                $gr->nextDay(true);
                 expect($gr->getItem(0)->quality)->toBe(8);
                 expect($gr->getItem(0)->sellIn)->toBe(9);
             });
             it('updates Conjured items at zero quality', function () {
                 $gr = new GildedRose([new Item('Conjured Mana Cake', 0, 10)]);
-                $gr->nextDay();
+                $gr->nextDay(true);
                 expect($gr->getItem(0)->quality)->toBe(0);
                 expect($gr->getItem(0)->sellIn)->toBe(9);
             });
             it('updates Conjured items on the sell date', function () {
                 $gr = new GildedRose([new Item('Conjured Mana Cake', 10, 0)]);
-                $gr->nextDay();
+                $gr->nextDay(true);
                 expect($gr->getItem(0)->quality)->toBe(6);
                 expect($gr->getItem(0)->sellIn)->toBe(-1);
             });
             it('updates Conjured items on the sell date at 0 quality', function () {
                 $gr = new GildedRose([new Item('Conjured Mana Cake', 0, 0)]);
-                $gr->nextDay();
+                $gr->nextDay(true);
                 expect($gr->getItem(0)->quality)->toBe(0);
                 expect($gr->getItem(0)->sellIn)->toBe(-1);
             });
             it('updates Conjured items after the sell date', function () {
                 $gr = new GildedRose([new Item('Conjured Mana Cake', 10, -10)]);
-                $gr->nextDay();
+                $gr->nextDay(true);
                 expect($gr->getItem(0)->quality)->toBe(6);
                 expect($gr->getItem(0)->sellIn)->toBe(-11);
             });
             it('updates Conjured items after the sell date at zero quality', function () {
                 $gr = new GildedRose([new Item('Conjured Mana Cake', 0, -10)]);
-                $gr->nextDay();
+                $gr->nextDay(true);
                 expect($gr->getItem(0)->quality)->toBe(0);
                 expect($gr->getItem(0)->sellIn)->toBe(-11);
             });
         });
-        */
     });
 });
--- src/GildedRose.php
+++ src/GildedRose.php
@@ -4,6 +4,10 @@ namespace App;
 
 class GildedRose
 {
+
+    const REGULAR_DEGRADATION_RATE = 1;
+    const CONJURED_DEGRADATION_RATE = 2;
+
     private $items;
 
     public function __construct(array $items)
@@ -20,15 +24,17 @@ class GildedRose
     }
 
     /**
-     * Test
+     * @param bool $conjured
      */
-    public function nextDay()
+    public function nextDay($conjured = false)
     {
+        $degradationRate = $conjured ? self::CONJURED_DEGRADATION_RATE : self::REGULAR_DEGRADATION_RATE;
+
         foreach ($this->items as $item) {
             if ($item->name != 'Aged Brie' and $item->name != 'Backstage passes to a TAFKAL80ETC concert') {
                 if ($item->quality > 0) {
                     if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                        $item->quality = $item->quality - 1;
+                        $item->quality = $item->quality - $degradationRate;
                     }
                 }
             } else {
@@ -56,7 +62,7 @@ class GildedRose
                     if ($item->name != 'Backstage passes to a TAFKAL80ETC concert') {
                         if ($item->quality > 0) {
                             if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                                $item->quality = $item->quality - 1;
+                                $item->quality = $item->quality - $degradationRate;
                             }
                         }
                     } else {
@@ -70,4 +76,5 @@ class GildedRose
             }
         }
     }
+
 }
