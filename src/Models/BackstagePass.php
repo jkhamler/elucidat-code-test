4885f2ed13ff962daf0bb2d577223de48850417d
Jonathan Hamler <jonathan.hamler@cazoo.co.uk>
2021-12-16 10:35:03 +0000

    Updating Product Classes

--- src/Models/BackstagePass.php
+++ src/Models/BackstagePass.php
@@ -12,20 +12,20 @@ class BackstagePass extends AbstractProduct
     /**
      * Quality starts doubling daily when there are this many days until the sell by date
      */
-    public const QUALITY_INCREMENTS_TWICE_AS_FAST = 10;
+    public const DAYS_BEFORE_SELL_BY_DATE_DOUBLE_QUALITY_DEGRADATION = 10;
 
     /**
      * Quality starts tripling daily when there are this many days until the sell by date
      */
-    public const QUALITY_INCREMENTS_THRICE_AS_FAST = 5;
+    public const DAYS_BEFORE_SELL_BY_DATE_TRIPLE_QUALITY_DEGRADATION = 5;
 
-    protected function changeQuality()
+    public function updateQuality()
     {
-        parent::changeQuality();
+        parent::updateQuality();
 
         // Quality increases 3 when there are 5 days or less
         if (
-            $this->sellIn <= self::QUALITY_INCREMENTS_THRICE_AS_FAST &&
+            $this->sellIn <= self::DAYS_BEFORE_SELL_BY_DATE_TRIPLE_QUALITY_DEGRADATION &&
             $this->quality < self::MIN_QUALITY
         ) {
             $this->quality += self::QUALITY_INCREMENTS;
--- src/Models/Sulfuras.php
+++ src/Models/Sulfuras.php
@@ -4,5 +4,23 @@ namespace App\Models;
 
 class Sulfuras
 {
+    /**
+     * "Sulfuras", a legendary item, never decreases in quality
+     */
+    public const SELL_IN_INCREMENTS = 0;
 
+    /**
+     * "Sulfuras", a legendary item, never decreases in quality
+     */
+    public const QUALITY_INCREMENTS = 0;
+
+    /**
+     * "Sulfuras", a legendary item, always has a quality of 80
+     */
+    public const QUALITY_MIN = 80;
+
+    /**
+     * "Sulfuras", a legendary item, always has a quality of 80
+     */
+    public const QUALITY_MAX = 80;
 }
